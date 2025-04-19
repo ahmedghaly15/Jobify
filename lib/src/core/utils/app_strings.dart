6c2eff3f114a4d1042d64ece8f97fc249597fd36
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-04-20 00:42:17 +0200

    Added AppRouterObserver

--- lib/src/core/utils/app_strings.dart
+++ lib/src/core/utils/app_strings.dart
@@ -2,4 +2,11 @@ class AppStrings {
   AppStrings._();
 
   static const String fontFamily = 'Inter';
+  static const String job = 'Job';
+  static const String appTitle = 'Jobify';
+  static const String tracking = 'Tracking';
+  static const String app = 'App';
+  static const String onboardingDescription =
+      "Jobify is a powerful and intuitive platform designed to streamline your job search and application process. Whether you're actively job hunting, keeping track of applications, or organizing opportunities, Jobify helps you stay on top of your career journey with ease.";
+  static const String getStarted = 'Get Started';
 }
