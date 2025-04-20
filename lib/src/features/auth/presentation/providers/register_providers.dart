594db731d85739119d098aaba1689ed8d0048704
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-04-20 12:31:34 +0200

    Finished register UI logic ✅

--- lib/src/features/auth/presentation/providers/register_providers.dart
+++ lib/src/features/auth/presentation/providers/register_providers.dart
@@ -1,7 +1,14 @@
+// ignore_for_file: avoid_manual_providers_as_generated_provider_dependency
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+import '../../../../core/models/jobify_user.dart';
+import '../../data/models/auth_request_params.dart';
+import '../../data/repos/register_repo.dart';
 import 'form_notifier_providers.dart';
+
+part 'register_providers.g.dart';
 
 final registerFormKeyProvider = Provider.autoDispose<GlobalKey<FormState>>(
   (ref) => GlobalKey<FormState>(),
@@ -39,3 +46,37 @@ final confirmPassObscureTextProvider =
     StateNotifierProvider.autoDispose<ToggleBoolNotifier, bool>(
       (ref) => ToggleBoolNotifier(),
     );
+
+@riverpod
+class Register extends _$Register {
+  @override
+  AsyncValue<JobifyUser>? build() {
+    return null;
+  }
+
+  void _register() async {
+    state = const AsyncLoading();
+    final result = await ref
+        .read(registerRepoProvider)
+        .auth(
+          AuthRequestParams(
+            email: ref.read(registerEmailControllerProvider).text.trim(),
+            password: ref.read(registerPassControllerProvider).text,
+            name: ref.read(registerNameControllerProvider).text.trim(),
+          ),
+        );
+    result.when(
+      success: (jobifyUser) => state = AsyncData(jobifyUser),
+      failure: (error) => state = AsyncError(error.message, StackTrace.current),
+    );
+  }
+
+  void validateFormAndRegister() {
+    final formKey = ref.read(registerFormKeyProvider);
+    if (formKey.currentState!.validate()) {
+      _register();
+    } else {
+      ref.read(registerAutovalidateModeProvider.notifier).enableAutoValidate();
+    }
+  }
+}
---
+++ lib/src/features/auth/presentation/providers/register_providers.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'register_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$registerHash() => r'a7e9afb35c2d498ef8ce2ee5dbf759047e00108d';
+
+/// See also [Register].
+@ProviderFor(Register)
+final registerProvider =
+    AutoDisposeNotifierProvider<Register, AsyncValue<JobifyUser>?>.internal(
+      Register.new,
+      name: r'registerProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product') ? null : _$registerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$Register = AutoDisposeNotifier<AsyncValue<JobifyUser>?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
