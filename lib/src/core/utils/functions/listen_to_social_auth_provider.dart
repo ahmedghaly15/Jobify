bdb8f6f40ad7346ae28df35a0006f312660aeb9c
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-04-21 21:37:51 +0200

    Built listenToSocialAuthProvider() function

---
+++ lib/src/core/utils/functions/listen_to_social_auth_provider.dart
@@ -0,0 +1,36 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:jobify/src/core/router/app_router.dart';
+
+import '../../helpers/extensions.dart';
+
+/// Listens to the given [provider] and when the [AsyncValue] is in either
+/// [AsyncValue.data] or [AsyncValue.error] state, takes the following actions:
+///
+/// - If the value is in [AsyncValue.data] state, it waits for 1.5 seconds
+///   and then navigates to the [HomeRoute] route.
+/// - If the value is in [AsyncValue.error] state, it shows an animated dialog
+///   with the given error message.
+void listenToSocialAuthProvider(
+  WidgetRef ref,
+  BuildContext context,
+  ProviderListenable<AsyncValue<dynamic>?> provider, {
+  bool shouldAwaitBeforeNavigation = true,
+}) {
+  ref.listen(provider, (_, current) {
+    current?.whenOrNull(
+      data: (_) async {
+        if (shouldAwaitBeforeNavigation) {
+          await Future.delayed(const Duration(milliseconds: 1500));
+        }
+        context.router.replaceAll([const HomeRoute()]);
+      },
+      error:
+          (error, _) => context.showAnimatedDialog(
+            state: CustomDialogStates.error,
+            message: error.toString(),
+          ),
+    );
+  });
+}
