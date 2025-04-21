fcb422134db8fe9fdf821f5b965cce06e5cc95bf
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-04-21 18:44:46 +0200

    Refactored SocialIcons widget

--- lib/src/features/auth/presentation/views/widgets/social_icons.dart
+++ lib/src/features/auth/presentation/views/widgets/social_icons.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
-import '../../../../../core/theming/app_colors.dart';
 import '../../../../../core/utils/app_assets.dart';
+import 'facebook_consumer_button.dart';
+import 'social_icon_widget.dart';
 
 class SocialIcons extends StatelessWidget {
   const SocialIcons({super.key});
@@ -12,24 +12,22 @@ class SocialIcons extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: List.generate(
-        _socialIcons.length,
-        (index) => Container(
-          margin: EdgeInsetsDirectional.only(start: index == 0 ? 0 : 32.w),
-          child: CircleAvatar(
-            radius: 25.r,
-            backgroundColor: AppColors.colorF5F5F5,
-            child: SvgPicture.asset(_socialIcons[index], fit: BoxFit.cover),
-          ),
+      spacing: 32.w,
+      children: [
+        SocialIconWidget(
+          assetPath: Assets.svgsGoogle,
+          onPressed: () {
+            // Handle Google sign-in
+          },
         ),
-        growable: false,
-      ),
+        const FacebookConsumerButton(),
+        SocialIconWidget(
+          assetPath: Assets.svgsLinkedin,
+          onPressed: () {
+            // Handle LinkedIn sign-in
+          },
+        ),
+      ],
     );
   }
 }
-
-const List<String> _socialIcons = [
-  Assets.svgsGoogle,
-  Assets.svgsFacebook,
-  Assets.svgsLinkedin,
-];
