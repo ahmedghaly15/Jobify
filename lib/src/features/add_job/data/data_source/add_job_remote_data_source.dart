aa3b2157fcb8170dd9725cdd6c140d195d17e516
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-05-03 16:15:28 +0300

    Updated addJob() method

--- lib/src/features/add_job/data/data_source/add_job_remote_data_source.dart
+++ lib/src/features/add_job/data/data_source/add_job_remote_data_source.dart
@@ -3,6 +3,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../../../core/supabase/supabase_request_result.dart';
 import '../../../../core/utils/const_strings.dart';
+import '../../../../core/utils/constants.dart';
 import '../models/add_job_request_body.dart';
 
 final addJobRemoteDataSourceProvider =
@@ -16,8 +17,40 @@ class AddJobRemoteDataSource {
   AddJobRemoteDataSource(this._supabaseClient);
 
   Future<void> addJob(AddJobRequestBody requestBody) async {
+    final results = await _fetchJobs();
+    if (results.isEmpty) {
+      await _insertFirstJob(requestBody);
+    } else {
+      await _updateJobs(results, requestBody);
+    }
+  }
+
+  Future<PostgrestList> _fetchJobs() async {
+    return await _supabaseClient
+        .from(ConstStrings.jobsTable)
+        .select()
+        .eq('user_id', currentUser!.user!.id);
+  }
+
+  Future<void> _updateJobs(
+    PostgrestList results,
+    AddJobRequestBody requestBody,
+  ) async {
+    final existingJobs = results.first['jobs'] as List<dynamic>? ?? [];
+    existingJobs.add(requestBody.toJson());
     await _supabaseClient
         .from(ConstStrings.jobsTable)
-        .insert(requestBody.toJson());
+        .update({'jobs': existingJobs})
+        .eq('user_id', currentUser!.user!.id);
+  }
+
+  Future<void> _insertFirstJob(AddJobRequestBody requestBody) async {
+    await _supabaseClient
+        .from(ConstStrings.jobsTable)
+        .insert({
+          'user_id': currentUser!.user!.id,
+          'jobs': [requestBody.toJson()],
+        })
+        .eq('user_id', currentUser!.user!.id);
   }
 }
