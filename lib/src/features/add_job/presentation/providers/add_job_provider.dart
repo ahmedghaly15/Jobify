cb55be2e64b436364f486ba3f72a9f58b3e60808
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-05-03 17:05:29 +0300

    Added new providers to add_job_providers file

--- lib/src/features/add_job/presentation/providers/add_job_provider.dart
+++ lib/src/features/add_job/presentation/providers/add_job_provider.dart
@@ -1,12 +1,19 @@
 // ignore_for_file: avoid_manual_providers_as_generated_provider_dependency
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+import '../../../../core/utils/constants.dart';
+import '../../../auth/presentation/providers/form_notifier_providers.dart';
 import '../../data/models/add_job_request_body.dart';
 import '../../data/repo/add_job_repo.dart';
 
 part 'add_job_provider.g.dart';
 
+final addJobAutovalidateModeProvider = StateNotifierProvider.autoDispose<
+  AutovalidateModeNotifier,
+  AutovalidateMode
+>((ref) => AutovalidateModeNotifier());
 final addJobFormKeyProvider = Provider.autoDispose<GlobalKey<FormState>>(
   (ref) => GlobalKey<FormState>(),
 );
@@ -25,6 +32,10 @@ final companyFocusNodeProvider = Provider.autoDispose<FocusNode>(
 final locationFocusNodeProvider = Provider.autoDispose<FocusNode>(
   (ref) => FocusNode(),
 );
+final statusProvider = StateProvider<String>(
+  (ref) => AppConstants.jobStatus.first,
+);
+final modeProvider = StateProvider<String>((ref) => AppConstants.jobMode.first);
 
 @riverpod
 class AddJob extends _$AddJob {
@@ -42,9 +53,8 @@ class AddJob extends _$AddJob {
             position: ref.read(positionControllerProvider).text,
             company: ref.read(companyControllerProvider).text,
             location: ref.read(locationControllerProvider).text,
-            // TODO: handle this
-            status: 'ref.read(statusControllerProvider).text',
-            mode: 'ref.read(modeControllerProvider).text',
+            status: ref.read(statusProvider),
+            mode: ref.read(modeProvider),
           ),
         );
     result.when(
@@ -58,6 +68,8 @@ class AddJob extends _$AddJob {
   void validateAndAddJob() {
     if (ref.read(addJobFormKeyProvider).currentState!.validate()) {
       _addJob();
+    } else {
+      ref.read(addJobAutovalidateModeProvider.notifier).enableAutoValidate();
     }
   }
 }
