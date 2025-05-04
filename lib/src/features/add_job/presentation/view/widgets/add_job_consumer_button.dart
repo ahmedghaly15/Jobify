2a7bf527830d8e6094bdba63f1831325b6ce7cb6
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-05-04 16:21:30 +0300

    Updated listener of AddJobConsumerButton widget

--- lib/src/features/add_job/presentation/view/widgets/add_job_consumer_button.dart
+++ lib/src/features/add_job/presentation/view/widgets/add_job_consumer_button.dart
@@ -4,6 +4,7 @@ import 'package:jobify/src/core/helpers/extensions.dart';
 import 'package:jobify/src/features/home/presentation/providers/home_provider.dart';
 
 import '../../../../../core/utils/app_strings.dart';
+import '../../../../../core/utils/constants.dart';
 import '../../../../../core/widgets/adaptive_circular_progress_indicator.dart';
 import '../../../../../core/widgets/primary_button.dart';
 import '../../providers/add_job_provider.dart';
@@ -30,14 +31,7 @@ class AddJobConsumerButton extends ConsumerWidget {
     ref.listen(addJobProvider, (_, current) {
       current?.whenOrNull(
         data:
-            (_) {
-          context.showAnimatedDialog(
-              state: CustomDialogStates.success,
-              message: AppStrings.jobAddedSuccessfully,
-          );
-          // ignore: unused_result
-          ref.refresh(fetchJobsProvider.future);
-        },
+            (_) => _onSuccess(context, ref),
         error:
             (error, _) => context.showAnimatedDialog(
               state: CustomDialogStates.error,
@@ -45,5 +39,23 @@ class AddJobConsumerButton extends ConsumerWidget {
             ),
       );
     });
+  }
+
+  void _onSuccess(BuildContext context, WidgetRef ref) {
+    context.showAnimatedDialog(
+      state: CustomDialogStates.success,
+      message: AppStrings.jobAddedSuccessfully,
+    );
+    // ignore: unused_result
+    ref.refresh(fetchJobsProvider.future);
+    _resetControllers(ref);
+  }
+
+  void _resetControllers(WidgetRef ref) {
+    ref.read(positionControllerProvider).clear();
+    ref.read(companyControllerProvider).clear();
+    ref.read(locationControllerProvider).clear();
+    ref.read(statusProvider.notifier).state = AppConstants.jobStatus.first;
+    ref.read(modeProvider.notifier).state = AppConstants.jobMode.first;
   }
 }
