0ea0794013eb8feca10bcdbfeaf052b0400f07d1
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-05-07 16:23:27 +0300

    Finished search providers

--- lib/src/features/search/presentation/providers/search_providers.dart
+++ lib/src/features/search/presentation/providers/search_providers.dart
@@ -1,31 +1,42 @@
+// ignore_for_file: avoid_manual_providers_as_generated_provider_dependency
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:jobify/src/core/utils/enums.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../../../core/models/job.dart';
 import '../../data/models/search_params.dart';
 import '../../data/repo/search_repo.dart';
 
+part 'search_providers.g.dart';
 
 final filterStateProvider = StateProvider.autoDispose<SearchFilter>(
   (ref) => SearchFilter.all,
 );
 
-final filteredSearchProvider = FutureProvider.autoDispose
-    <List<Job>>((
-  ref,
-) async {
-      final searchRemoteResult = await ref
-          .read(searchRepoProvider)
-          .searchJobs(
-            SearchParams(
-          position: ref.watch(searchTextProvider),
-              filter: ref.watch(filterStateProvider),
-            ),
-          );
-      return searchRemoteResult.when(
-        success: (result) => result,
-        failure: (error) => throw error.message,
-      );
-    });
-
 final searchTextProvider = StateProvider.autoDispose<String>((ref) => '');
+
+@riverpod
+class FilteredSearch extends _$FilteredSearch {
+  @override
+  AsyncValue<List<Job>> build() {
+    return const AsyncValue.data([]);
+  }
+
+  void fetchFilteredSearch() async {
+    state = const AsyncValue.loading();
+    final searchRemoteResult = await ref
+        .read(searchRepoProvider)
+        .searchJobs(
+          SearchParams(
+            position: ref.watch(searchTextProvider),
+            filter: ref.watch(filterStateProvider),
+          ),
+        );
+    searchRemoteResult.when(
+      success: (result) => state = AsyncValue.data(result),
+      failure:
+          (error) =>
+              state = AsyncValue.error(error.message, StackTrace.current),
+    );
+  }
+}
