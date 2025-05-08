f8b47078887c7f8e121cc7d45b1603e6a18bf903
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-05-08 16:04:18 +0300

    Refactored AddJobFormConsumer widget

--- lib/src/features/add_job/presentation/view/widgets/add_job_form_card.dart
+++ lib/src/features/add_job/presentation/view/widgets/add_job_form_card.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'animated_add_job_form.dart';
+import '../../../../../core/theming/app_text_styles.dart';
+import '../../../../../core/utils/app_strings.dart';
+import 'add_job_form_consumer.dart';
 
 class AddJobFormCard extends StatelessWidget {
   const AddJobFormCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return Card(
       elevation: 8.h,
       color: Colors.white,
       child: Padding(
         padding: EdgeInsets.symmetric(vertical: 16.h, horizontal: 16.w),
-        child: const AnimatedAddJobForm(),
+        child: Column(
+          spacing: 16.h,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(AppStrings.addJob, style: AppTextStyles.font36SemiBold),
+            const AddJobFormConsumer(),
+          ],
+        ),
       ),
     );
   }
--- lib/src/features/add_job/presentation/view/widgets/add_job_form_consumer.dart
+++ lib/src/features/add_job/presentation/view/widgets/add_job_form_consumer.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jobify/src/core/helpers/extensions.dart';
 
 import '../../../../../core/helpers/field_validator.dart';
-import '../../../../../core/theming/app_text_styles.dart';
 import '../../../../../core/utils/app_strings.dart';
 import '../../../../../core/widgets/custom_text_form_field.dart';
 import '../../providers/add_job_provider.dart';
 import 'job_mode_dropdown_consumer.dart';
 import 'job_status_dropdown_consumer.dart';
 
-const _moveBegin = -50.0;
-const _moveEnd = 0.0;
-
-class AnimatedAddJobForm extends ConsumerWidget {
-  const AnimatedAddJobForm({super.key});
+class AddJobFormConsumer extends ConsumerWidget {
+  const AddJobFormConsumer({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -34,23 +29,10 @@ class AnimatedAddJobForm extends ConsumerWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         spacing: 16.h,
         children: [
-          Text(
-            AppStrings.addJob,
-            style: AppTextStyles.font36SemiBold,
-          ).animate().fadeIn().moveY(
-            begin: _moveBegin.h,
-            end: _moveEnd,
-            duration: 400.milliseconds,
-          ),
           CustomTextFormField(
             controller: positionController,
             hintText: AppStrings.position,
             validating: (value) => FieldValidator.validatingEmptyField(value),
-          ).animate().fadeIn().moveY(
-            begin: _moveBegin.h,
-            end: _moveEnd,
-            duration: 400.milliseconds,
-            delay: 200.milliseconds,
           ),
           CustomTextFormField(
             controller: companyController,
@@ -58,35 +40,15 @@ class AnimatedAddJobForm extends ConsumerWidget {
             focusNode: companyFocusNode,
             validating: (value) => FieldValidator.validatingEmptyField(value),
             onEditingComplete: () => context.requestFocus(locationFocusNode),
-          ).animate().fadeIn().moveY(
-            begin: _moveBegin.h,
-            end: _moveEnd,
-            duration: 400.milliseconds,
-            delay: 400.milliseconds,
           ),
           CustomTextFormField(
             controller: locationController,
             focusNode: locationFocusNode,
             hintText: AppStrings.location,
             validating: (value) => FieldValidator.validatingEmptyField(value),
-          ).animate().fadeIn().moveY(
-            begin: _moveBegin.h,
-            end: _moveEnd,
-            duration: 400.milliseconds,
-            delay: 600.milliseconds,
           ),
-          const JobStatusDropdownConsumer().animate().fadeIn().moveY(
-            begin: _moveBegin.h,
-            end: _moveEnd,
-            duration: 400.milliseconds,
-            delay: 800.milliseconds,
-          ),
-          const JobModeDropdownConsumer().animate().fadeIn().moveY(
-            begin: _moveBegin.h,
-            end: _moveEnd,
-            duration: 400.milliseconds,
-            delay: 1000.milliseconds,
-          ),
+          JobStatusDropdownConsumer(statusProvider: statusProvider),
+          JobModeDropdownConsumer(modeProvider: modeProvider),
         ],
       ),
     );
