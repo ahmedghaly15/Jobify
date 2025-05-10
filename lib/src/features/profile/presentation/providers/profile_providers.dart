8ed27b9012d482555aab25d9f0f0eb59886daf1a
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-05-10 14:45:21 +0300

    Updated profile providers

--- lib/src/features/profile/presentation/providers/profile_providers.dart
+++ lib/src/features/profile/presentation/providers/profile_providers.dart
@@ -8,22 +8,41 @@ final profileAutovalidateModeProvider = StateNotifierProvider.autoDispose<
   AutovalidateModeNotifier,
   AutovalidateMode
 >((ref) => AutovalidateModeNotifier());
+
 final profileFormKeyProvider = Provider.autoDispose<GlobalKey<FormState>>(
   (ref) => GlobalKey<FormState>(),
 );
+
 final profileEmailControllerProvider =
     Provider.autoDispose<TextEditingController>(
       (ref) => TextEditingController(text: currentUser?.user?.email),
     );
+
 final profileNameControllerProvider =
     Provider.autoDispose<TextEditingController>(
       (ref) => TextEditingController(text: currentUser?.name),
     );
+
+final profilePassControllerProvider =
+    Provider.autoDispose<TextEditingController>(
+      (ref) => TextEditingController(
+        text: currentUser?.user?.userMetadata?['password'],
+      ),
+    );
+
+final profilePassObscureTextProvider =
+    StateNotifierProvider.autoDispose<ToggleBoolNotifier, bool>(
+      (ref) => ToggleBoolNotifier(),
+    );
+
 final isUpdateProfileButtonEnabledProvider = StateProvider.autoDispose<bool>((
   ref,
 ) {
   final emailController = ref.watch(profileEmailControllerProvider);
   final nameController = ref.watch(profileNameControllerProvider);
+  final passController = ref.watch(profilePassControllerProvider);
   return emailController.text != currentUser?.user?.email ||
-      nameController.text != currentUser?.name;
+      nameController.text != currentUser?.name ||
+      nameController.text.isNotEmpty ||
+      passController.text.isNotEmpty;
 });
