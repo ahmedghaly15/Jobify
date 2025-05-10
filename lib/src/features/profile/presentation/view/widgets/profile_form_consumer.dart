f499f8be56264ab4f17b1b4853de173c4aef77eb
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-05-10 16:24:26 +0300

    Updated ProfileFormConsumer widget

--- lib/src/features/profile/presentation/view/widgets/profile_form_consumer.dart
+++ lib/src/features/profile/presentation/view/widgets/profile_form_consumer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../../../core/helpers/field_validator.dart';
 import '../../../../../core/widgets/email_text_form_field.dart';
 import '../../../../../core/widgets/name_text_form_field.dart';
 import '../../../../../core/widgets/password_text_form_field.dart';
@@ -14,8 +15,6 @@ class ProfileFormConsumer extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final formKey = ref.watch(profileFormKeyProvider);
     final autovalidateMode = ref.watch(profileAutovalidateModeProvider);
-    final emailController = ref.watch(profileEmailControllerProvider);
-    final nameController = ref.watch(profileNameControllerProvider);
     return Form(
       key: formKey,
       autovalidateMode: autovalidateMode,
@@ -23,8 +22,21 @@ class ProfileFormConsumer extends ConsumerWidget {
         spacing: 16.h,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          NameTextFormField(controller: nameController),
-          EmailTextFormField(controller: emailController),
+          NameTextFormField(
+            initialValue: ref.read(profileNameTextProvider),
+            onChanged: (value) {
+              ref
+                  .read(profileNameTextProvider.notifier)
+                  .update((state) => value);
+            },
+          ),
+          EmailTextFormField(
+            initialValue: ref.read(profileEmailTextProvider),
+            onChanged:
+                (value) => ref
+                    .read(profileEmailTextProvider.notifier)
+                    .update((state) => value),
+          ),
           const _ProfilePassFieldConsumer(),
         ],
       ),
@@ -37,14 +49,19 @@ class _ProfilePassFieldConsumer extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final controller = ref.watch(profilePassControllerProvider);
     final obscureText = ref.watch(profilePassObscureTextProvider);
     return PasswordTextFormField(
-      passController: controller,
+      initialValue: ref.read(profilePassTextProvider),
       obscureText: obscureText,
       suffixOnPressed: () {
         ref.read(profilePassObscureTextProvider.notifier).toggle();
       },
+      onChanged:
+          (value) => ref
+              .read(profilePassTextProvider.notifier)
+              .update((state) => value),
+      validating:
+          (value) => FieldValidator.validateOptionalPassword(value: value),
     );
   }
 }
